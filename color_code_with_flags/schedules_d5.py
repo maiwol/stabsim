e36ea8470fe2f717b2d0d27a59f1331becb183ce
mau <maiwol@gmail.com>
2017-06-08 16:42:19 +0100

    Markus pointed out that I was not considering all possible hook errors. Started writing functions to look for hooks in a more rigorous way.

--- color_code_with_flags/schedules_d5.py
+++ color_code_with_flags/schedules_d5.py
@@ -10,13 +10,13 @@ n_total = 17
 
 d5_stabs = [
               [2,3,6,5,10,9,13,14],
+              [6,7,10,11],
+              [10,11,14,15],
+              [7,11,15,16],
               [0,1,2,3],
               [0,2,4,5],
               [4,5,8,9],
-              [8,9,12,13],
-              [6,7,10,11],
-              [10,11,14,15],
-              [7,11,15,16]
+              [8,9,12,13]
            ]
 
 logical_oper = range(n_total)
@@ -31,7 +31,16 @@ for i in range(n_total):
         errors_2 += [[i,j]]
 
 total_errors = errors_0 + errors_1 + errors_2
-        
+    
+
+# Trivial lookuptable is the basis of every dictionary.
+# It includes only the trivial syndrome.
+trivial_lookup = {}
+for err in errors_0:
+    err_bin = sched_fun.convert_to_binary(err, n_total)
+    syn = tuple(sched_fun.error_to_syndrome(err_bin, n_total, d5_stabs[:]))
+    trivial_lookup[syn] = err_bin
+    
 # Basic lookuptable is the basis of every dictionary.
 # It includes the trivial syndrome and all 1-q errors.
 basic_lookup = {}
@@ -41,9 +50,19 @@ for err in errors_0 + errors_1:
     basic_lookup[syn] = err_bin
 
 
+# dict_noflag is the dictionary when no flags are triggered.
+# First, we add the I, 1-q, and 2-q errors.
+dict_noflag = {}
+for err in total_errors:
+    err_bin = sched_fun.convert_to_binary(err, n_total)
+    syn = tuple(sched_fun.error_to_syndrome(err_bin, n_total, d5_stabs[:]))
+    dict_noflag[syn] = err_bin
 
 
-def basic_dict1(sched, flags=[1,3], n_total=17, stabs=d5_stabs[:]):
+
+
+def basic_dict1(sched, flags=[1,3], n_total=17, stabs=d5_stabs[:],
+                extra_errors=[]):
     '''
     computes the basic dictionary when the stabilizer given by
     sched triggers a flag.  Basic dictionary means that we don't
@@ -54,16 +73,37 @@ def basic_dict1(sched, flags=[1,3], n_total=17, stabs=d5_stabs[:]):
     gates from the ancilla qubit to the flag qubit.  
     For example, [1,3] means that the first CNOT occurs before the 
     second gate and the second CNOT occurs before the third gate.
-    
+   
+    extra_errors are other errors, apart from the 1-q erros, that
+    can occur with probability O(p).  For example, when the octagon
+    has flags [2,6], (0,1) and (6,7) are extra_errors.
+
     returns True, dict1  if it's possible to construct the dict
             False, {}    if it's not possible.
     '''
 
+    sched = list(sched)
+
     dict1 = dict(basic_lookup)
+    for extra_error in extra_errors:
+        extra_bin = sched_fun.convert_to_binary(extra_error, n_total)
+        syn = tuple(sched_fun.error_to_syndrome(extra_bin, n_total, stabs[:]))
+        if syn not in dict1:
+            dict1[syn] = extra_bin
+        else:
+            after_corr, num_corr = sched_fun.correct_until_in_codespace(extra_bin,
+                                                                        dict1,
+                                                                        n_total,
+                                                                        stabs[:])
+            log_parity = sched_fun.overlapping_parity(after_corr, tuple(log_bin))
+            if log_parity == 1:
+                return False, {}
+        
+
     for i in range(flags[0], flags[1]+1):
         hook = sched[i : ]
         hook_bin = sched_fun.convert_to_binary(hook, n_total)
-        for err in errors_0 + errors_1:
+        for err in errors_0 + errors_1 + extra_errors:
             err_bin = sched_fun.convert_to_binary(err, n_total)
             comb_err = sched_fun.multiply_operators(hook_bin, err_bin)
             syn = tuple(sched_fun.error_to_syndrome(comb_err, n_total, stabs[:]))
@@ -85,6 +125,8 @@ def basic_dict1(sched, flags=[1,3], n_total=17, stabs=d5_stabs[:]):
 def basic_dict2(sched1, flags1, sched2, flags2, n_total=17, stabs=d5_stabs[:]):
     '''
     '''
+
+    sched1, sched2 = list(sched1), list(sched2)
 
     # (no hook)1 (no hook)2
     dict2 = {tuple([0 for i in range(len(stabs))]): tuple([0 for i in range(n_total)])}
@@ -145,7 +187,7 @@ def dict2_for_every_sched(good_scheds, flags_good, schedn, flagn,
              else:                     False
     '''
 
-    for i in len(good_scheds):
+    for i in range(len(good_scheds)):
         exists2, dict2 = basic_dict2(good_scheds[i][:], flags_good[i][:],
                                      schedn[:], flagn[:],
                                      n_total, stabs)
@@ -156,51 +198,170 @@ def dict2_for_every_sched(good_scheds, flags_good, schedn, flagn,
 
 
 
-def add_next_sched(list_good_scheds, flags_good, stabn, flagn,
-                   n_total=17, stabs=d5_stabs[:]):
+def add_next_sched(list_good_scheds, flags_good, stabn, flagsn,
+                   n_total=17, stabs=d5_stabs[:], perms_important=[],
+                   extra_errors=[]):
     '''
+    Each schedule is a tuple
+
     list_good_scheds is a list of all the good schedules found so far:
     [[sched1, sched2, ...], [sched1, sched2, ...], ...]
     '''
 
-    for schedn in it.permutations(stabn):
-        schedn = list(schedn)
+    new_good_scheds = []
+    #for schedn in it.permutations(stabn):
+    for perm in perms_important:
+        schedn = [stabn[q] for q in perm]
+        #schedn = list(schedn)
         
-        # First compute dict1 for the new schedule
-        exists1, dict1 = basic_dict1(schedn[:], flagsn[:], n_total, stabs)
-        if exists1:
-            # if dict1 exists, then check that there's a dict2 between the
-            # new schedule and every schedule in the list of good schedules
-            for good_scheds in list_good_scheds:
+        for good_scheds in list_good_scheds:
+            # First compute dict1 for the new schedule
+            # Need to add the extra_errors
+            # This is inefficient.
+            errors_oct = [[good_scheds[0][q] for q in err] for err in extra_errors]
+            exists1, dict1 = basic_dict1(schedn[:], flagsn[:], n_total,
+                                         stabs, errors_oct)
+            if exists1:
+                # if dict1 exists, then check that there's a dict2 between the
+                # new schedule and every schedule in the list of good schedules
                 exists2 = dict2_for_every_sched(good_scheds[:], flags_good[:],
-                                                schedn[:], flagn[:],
+                                                schedn[:], flagsn[:],
                                                 n_total, stabs)
     
                 if exists2:
-                    new_good_scheds += [good_scheds + schedn] 
+                    new_good_scheds += [good_scheds[:] + [schedn]] 
 
     return new_good_scheds
 
 
-#infile_name = 'schedules_octagon.json'
-#infile = open(infile_name, 'r')
-#good_schedules = json.load(infile).values()
-#infile.close()
+
+# First we obtain the good schedules for the octagon
+#flags_oct = [2,6]
+# When flags_oct = [2,6], the schedules have mirror
+# symmetry in the sense that the reversed schedule
+# has the same hook errors.
+'''
+perms_important, perms_reversed = [], []
+for perm in it.permutations(range(8)):
+    if perm not in perms_reversed:
+        perms_important += [perm]
+        perms_reversed += [tuple(reversed(perm))]
 
 n_good = 0
 sched_dict = {}
-for perm in it.permutations(d5_stabs[0]):
-    perm = list(perm)
-    exists, dict1 = basic_dict1(perm, [2,7])
+for perm in perms_important:
+    sched = [d5_stabs[0][i] for i in perm]
+    exists, dict1 = basic_dict1(sched, flags_oct)
     if exists:
-        sched_dict[n_good] = perm
+        sched_dict[n_good] = sched
         n_good += 1
 
-#outfile_name = 'schedules_octagon2.json'
-#outfile = open(outfile_name, 'w')
-#json.dump(sched_dict, outfile, indent=4, separators=(',', ':'),
-#          sort_keys=True)
-#outfile.close()
+outfile_name = 'schedules_octagon_optimized.json'
+outfile = open(outfile_name, 'w')
+json.dump(sched_dict, outfile, indent=4, separators=(',', ':'),
+          sort_keys=True)
+outfile.close()
+print 'good schedules octagon =', n_good
+'''
 
 
-print n_good
+# First we load the schedules we had obtained for the octagon
+# There are 2880 of them if we choose flags = [2,6].
+
+'''
+infile_name = 'schedules_octagon_optimized.json'
+infile = open(infile_name, 'r')
+good_schedules_oct = json.load(infile).values()
+infile.close()
+list_good_schedules = []
+for sched in good_schedules_oct:
+    list_good_schedules += [[tuple(sched)]]
+
+flags_good = [flags_oct[:]]
+flags_sq = [1,3]
+extra_oct_errors = [[0,1], [6,7]]
+
+perms_important_sq, perms_reversed_sq = [], []
+for perm in it.permutations(range(4)):
+    if perm not in perms_reversed_sq:
+        perms_important_sq += [perm]
+        perms_reversed_sq += [tuple(reversed(perm))]
+
+#perms_important_sq = [(0,1,2,3)]
+for stabilizer in d5_stabs[1:]:
+    list_good_schedules = add_next_sched(list_good_schedules, flags_good[:],
+                                         stabilizer, flags_sq[:], 
+                                         n_total, d5_stabs[:],
+                                         perms_important_sq, extra_oct_errors)
+    flags_good += [flags_sq[:]]
+    outfile_name = 'other_schedules%i.json' %d5_stabs.index(stabilizer)
+    outfile = open(outfile_name, 'w')
+    good_schedules_dict = {}
+    for i in range(len(list_good_schedules)):
+        good_schedules_dict[i] = list_good_schedules[i]
+    json.dump(good_schedules_dict, outfile, indent=4, separators=(',', ':'),
+              sort_keys=True)
+    outfile.close()
+
+    print '%i: %i' %(d5_stabs.index(stabilizer), len(list_good_schedules))
+'''
+
+def hook_and_flag(sched_flags, err_locs, flag='f1', n_total=17):
+    '''
+    - sched_flags is a schedule with a flag.  For example:
+    [2,3,'f1',5,6,9,10,'f1',13,14,'m1']
+    - err_locs are the indexes of the locations of the errors
+    - flag is the flag we are interested in, cause we might
+    have more than two for the octagon.
+
+    returns:  trigger (0 or 1), the hook error
+    '''
+
+    meas = 'm' + flag[1]
+    flag_indexes = [i for i,q in enumerate(sched_flags) if q==flag]
+    total_hook_bin = [0 for i in range(n_total)]
+    trigger = 0
+    for err_loc in err_locs:
+        local_hook_flags = sched_flags[err_loc]
+        if local_hook_flags.count(flag)%2==1 or sched_flags[err_loc]==meas:
+            trigger += 1
+        local_hook = [q for q in local_hook_flags if type(q)==type(0)]
+        local_hook_bin = sched_fun.convert_to_binary(local_hook, n_total)
+        total_hook_bin = sched_fun.multiply_operators(total_hook_bin, 
+                                                      local_hook_bin)
+
+    return trigger, total_hook_bin
+
+
+
+def all_hooks(sched_flags, flags=['f1'], err_weight=1, n_total=17):
+    '''
+    for a schedule with flags, it calculates all the possible
+    hook errors that are caused by an error event of 
+    weight err_weight and whether or not they trigger each flag.
+    This is only for a particular schedule of a given stabilizer.
+
+    It returns a dictionary of flag triggering outcomes and hooks.
+    '''
+    
+    dict_hooks = {}
+    for prod in it.product([0,1], repeat=len(flags)):
+        dict_hooks[prod] = []
+
+    for err_locs in it.combinations(range(len(sched_flags)), err_weight):
+        err_locs = list(err_locs)
+        triggers, hooks = [], []
+        for flag in flags:
+            trigger, hook = hook_and_flag(sched_flags[:], err_locs, 
+                                          flag, n_total)
+            triggers += [trigger]
+        # for a given error location, the hook will always be the same;
+        # only the triggering pattern will change.
+        dict_hooks[tuple(triggers)] += [hook]
+
+    return dict_hooks
+
+
+
+def hooks_w2(sched_flags, flags=['f1'])
+
